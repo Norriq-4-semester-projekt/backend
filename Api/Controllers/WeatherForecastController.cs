5616c3a691357d0d4f67505b9cc00c5639ac23a4
Michael Hatlehol Jensen <1010959@ucn.dk>
2021-03-02 09:13:27 +0100

    serilog

--- Api/Controllers/WeatherForecastController.cs
+++ Api/Controllers/WeatherForecastController.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Api.Controllers
 {
@@ -24,16 +22,35 @@ namespace Api.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IActionResult Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            try
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                // https://youtu.be/0acSdHJfk64?list=PLUOequmGnXxOFPJv8H7DNIappcta9brtN
+
+                //Proof of concept
+
+                //_logger.LogInformation(message: "First Log");
+                var rng = new Random();
+
+                if ((rng.Next(0, 5) < 2))
+                {
+                    throw new Exception(message: "Random Bad Call");
+                } 
+                return Ok(
+                     Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                     {
+                         Date = DateTime.Now.AddDays(index),
+                         TemperatureC = rng.Next(-20, 55),
+                         Summary = Summaries[rng.Next(Summaries.Length)]
+                     })
+                .ToArray());
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception, "Something bad happened");
+                return new StatusCodeResult(500);
+            }
         }
     }
 }
