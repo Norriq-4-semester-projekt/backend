e0380a5b569f81f633ac31fe7dbf3f1461d66bd7
spiril1000 <martin.jokumsen@gmail.com>
2021-03-05 14:11:26 +0100

    Kan ikke helt huske hvad der er i det her commit :pPPPPPPP

--- Api/Controllers/AuthController.cs
+++ Api/Controllers/AuthController.cs
@@ -6,7 +6,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using Nest;
 using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,7 +17,7 @@ namespace Api.Controllers
     // [ApiVersion("0.9", Deprecated = true)] // Set previous version as deprecated
     [ApiVersion("1.0")] // Set version of controller
     [ApiController]
-    [Route("api/v{version:apiVersion}/[controller]")]
+    [Route("v{version:apiVersion}/[action]")]
     public class AuthController : Controller
     {
         private readonly IConfiguration Configuration;
@@ -26,25 +28,60 @@ namespace Api.Controllers
         }
 
         [HttpGet]
-        public string Login()
+        public async Task<ActionResult> Login(String Username, String Password)
         {
-            return this.GenerateJWTToken(); ;
+            User u = new User(Username);
+            var settings = new ConnectionSettings(new Uri("http://164.68.106.245:9200")).DefaultIndex("users*");
+            var client = new ElasticClient(settings);
+
+            try
+            {
+                var rs = await client.SearchAsync<User>(s => s
+                    .Query(q => q
+                        .MatchPhrase(mp => mp
+                                    .Field("username").Query(Username))));
+                if (rs.Hits.Count > 0)
+                {
+                foreach (IHit<User> user in rs.Hits)
+                {
+                        User us = user as User;
+                        if(PasswordHelper.ComparePass(Password, us.PasswordHash, us.Salt))
+                        {
+                            return Ok(GenerateJWTToken(us));
+                        }
+                        
+                }
+                }
+                    return new StatusCodeResult(500);
+            }
+            catch (Exception)
+            {
+                //_logger.LogError(exception, "Could not retrieve any data from ElasticSearch");
+                return new StatusCodeResult(500);
+            }
+
         }
 
-        private string GenerateJWTToken()
+        private string GenerateJWTToken(User user)
         {
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JwtToken:SecretKey"]));
             var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
             var issuer = Configuration["JwtToken:Issuer"];
             var audience = Configuration["JwtToken:Audience"];
             var timeToLive = DateTime.Now.AddMinutes(Convert.ToDouble(Configuration["JwtToken:TokenExpiry"]));
+            var claims = new List<Claim>
+            {
+                new Claim("username", user.Username),
+                new Claim("LoggedOn", DateTime.Now.ToString())
+            };
 
             //ToDo add claims with user info!
             var token = new JwtSecurityToken(
             issuer,
             audience,
             expires: timeToLive,
-            signingCredentials: credentials
+            signingCredentials: credentials,
+            claims: claims
             );
 
             return new JwtSecurityTokenHandler().WriteToken(token);
