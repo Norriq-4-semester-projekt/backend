a5a9c47c11562f2241838fda6e9bf5aad2d1596d
Michael Hatlehol Jensen <1010959@ucn.dk>
2021-03-23 14:52:29 +0100

    getnetworkcpudata

--- Api/Controllers/v1/WeatherForecastController.cs
+++ Api/Controllers/v1/WeatherForecastController.cs
@@ -275,7 +275,7 @@ namespace Api.Controllers.v1_0
                         .Filter(f => f
                             .DateRange(dr => dr
                                 .Field("@timestamp")
-                                .GreaterThanOrEquals("now-5m")
+                                .GreaterThanOrEquals("now-2d")
                                 )
                             )
                         )
@@ -302,6 +302,108 @@ namespace Api.Controllers.v1_0
                 {
                     Console.WriteLine(response.DebugInformation);
                     var dateHistogram = response.Aggregations.DateHistogram("myNetworkDateHistogram");
+                    List<Object> list = new List<Object>();
+                    foreach (DateHistogramBucket item in dateHistogram.Buckets)
+                    {
+                        Dictionary<string, dynamic> newlist = new Dictionary<string, dynamic>();
+                        newlist.Add("Timestamp", item.KeyAsString);
+
+                        foreach (var item2 in item.Keys)
+                        {
+                            item.TryGetValue(item2, out IAggregate a);
+                            ValueAggregate valueAggregate = a as ValueAggregate;
+                            newlist.Add(item2, valueAggregate.Value);
+                        }
+                        list.Add(newlist);
+                    }
+                    return Ok(JsonSerializer.Serialize(list));
+                }
+                return new StatusCodeResult(200);
+            }
+            catch (Exception)
+            {
+                return new StatusCodeResult(500);
+            }
+        }
+        [HttpGet]
+        public async Task<ActionResult> GetNetworkCpuTrafic()
+        {
+            var settings = new ConnectionSettings(new Uri("http://164.68.106.245:9200")).DefaultIndex("metricbeat-*");
+            settings.ThrowExceptions(alwaysThrow: true); // I like exceptions
+            settings.PrettyJson(); // Good for DEBUG
+            settings.BasicAuthentication("elastic", "changeme");
+            settings.DisableDirectStreaming();
+            var client = new ElasticClient(settings);
+            try
+            {
+                    var response = await client.SearchAsync<dynamic>(s => s
+                    .Size(0)
+                    .Query(q => q
+                        .Bool(b => b
+                            .Should(sh => sh
+                                .MatchPhrase(mp => mp
+                                    .Field("hostname").Query("vmi316085.contaboserver.net")
+                                    .Field("event.dataset").Query("system.network")
+                                )
+                            )
+                            .Filter(f => f
+                                .DateRange(dr => dr
+                                    .Field("@timestamp")
+                                    .GreaterThanOrEquals("now-2d")
+                                    )
+                                )
+                            )
+                        )
+                    .Query(q => q
+                        .Bool(b => b
+                              .Should(sh => sh
+                                    .MatchPhrase(mp => mp
+                                        .Field("host.name").Query("vmi316085.contaboserver.net")
+                                        .Field("event.dataset").Query("system.cpu")
+                                        )
+                                    )
+                                .Filter(f => f
+                                    .DateRange(dr => dr
+                                    .Field("@timestamp")
+                                    .GreaterThanOrEquals("now-2d")
+                                    )
+                                )
+                            )
+                        )
+                    .Aggregations(aggs => aggs
+                        .DateHistogram("myNetworkCpuDateHistogram", date => date
+                        .Field("@timestamp")
+                        .CalendarInterval(DateInterval.Minute)
+                        .Aggregations(aggs => aggs
+                            .Average("AvgUserCpu", auc => auc
+                            .Field("system.cpu.user.pct"))
+                            .Average("AvgSystemCpu", asc => asc
+                            .Field("system.cpu.system.pct"))
+                            .Max("CpuCoresMax", mcpu => mcpu
+                            .Field("system.cpu.cores"))
+                            .Average("AVGnetIN", avg => avg
+                            .Field("host.network.in.bytes"))
+                            .Average("AVGnetOut", avg => avg
+                            .Field("host.network.out.bytes"))
+                            .Max("MAXnetIN", max => max
+                            .Field("host.network.in.bytes"))
+                            .Max("MAXnetOUT", max => max
+                            .Field("host.network.out.bytes"))
+                        .BucketScript("CpuCalc", bs => bs
+                            .BucketsPath(bp => bp
+                            .Add("user", "AvgUserCpu")
+                            .Add("system", "AvgSystemCpu")
+                            .Add("cores", "CpuCoresMax"))
+                            .Script("(params.user + params.system) / params.cores"))
+                            )
+                        )
+                    )
+                );
+
+                if (response.Aggregations.Count > 0)
+                {
+                    Console.WriteLine(response.DebugInformation);
+                    var dateHistogram = response.Aggregations.DateHistogram("myNetworkCpuDateHistogram");
                     List<Object> list = new List<Object>();
                     foreach (DateHistogramBucket item in dateHistogram.Buckets)
                     {
